0ca9c28d39b541adb4e8e4545c31cca906cae9b5
Spastic <junior.tan@live.com>
2023-04-16 06:07:42 +0800

    deleted android folder and re-ran flutter create

--- android/app/src/main/java/com/example/quizapp/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.quizapp;
-
-import io.flutter.embedding.android.FlutterActivity;
-//https://github.com/flutter/flutter/wiki/Upgrading-pre-1.12-Android-projects
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/advanced_flutter_firestore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.advanced_flutter_firestore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
